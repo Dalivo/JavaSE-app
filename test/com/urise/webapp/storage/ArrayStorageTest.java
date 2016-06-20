4cf902fa8b67dcab9f4618caa3068e555c89d888
Dalivo <dalivo@ukr.net>
2016-06-20 15:58:24 +0300

    hw5

---
+++ test/com/urise/webapp/storage/ArrayStorageTest.java
@@ -0,0 +1,12 @@
+package com.urise.webapp.storage;
+
+/**
+ * gkislin
+ * 12.06.2016
+ */
+public class ArrayStorageTest extends AbstractArrayStorageTest {
+
+    public ArrayStorageTest() {
+        super(new ArrayStorage());
+    }
+}
