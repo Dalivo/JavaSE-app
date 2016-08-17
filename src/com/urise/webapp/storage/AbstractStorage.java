3e6eec7d9e8aae0495ca44903a0b1f9d6cb70846
Dalivo <dalivo@ukr.net>
2016-08-17 20:55:15 +0300

    HW9

--- src/com/urise/webapp/storage/AbstractStorage.java
+++ src/com/urise/webapp/storage/AbstractStorage.java
@@ -12,6 +12,7 @@ import java.util.logging.Logger;
 public abstract class AbstractStorage<SK> implements Storage {
 
 //    protected final Logger LOG = Logger.getLogger(getClass().getName());
+
     private static final Logger LOG = Logger.getLogger(AbstractStorage.class.getName());
 
     protected abstract SK getSearchKey(String uuid);
